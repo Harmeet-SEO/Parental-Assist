6b2da396d6270b0bd31fc730b24c328e44184b36
Harmeet Singh <bhootu2306@gmail.com>
2025-07-02 16:39:25 -0400

    made some changes in home page

--- frontend/src/components/HomeBanner.jsx
+++ frontend/src/components/HomeBanner.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./HomeBanner.css";
 
 const HomeBanner = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="banner">
       <div className="banner-text">
@@ -9,7 +12,9 @@ const HomeBanner = () => {
         <p>
           Your go-to space for parenting tips, podcasts, and peace of mind 💡
         </p>
-        <button className="banner-btn">Explore Articles</button>
+        <button className="banner-btn" onClick={() => navigate("/articles")}>
+          Explore Articles
+        </button>
       </div>
     </div>
   );
